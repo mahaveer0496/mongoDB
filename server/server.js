66b53d4062fef602c1d280d1fdfd8ef6b237930d
Mahaveer <mahaveersharma0496@yahoo.com>
2017-04-16 21:23:43 +0530

    linting and further mongoose exploration :)

--- server/server.js
+++ server/server.js
@@ -1,3 +1,65 @@
-import * as mongoose from 'mongoose';
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongoose = require('./db/mongoose');
+var Todo = require('./models/Todo');
+var User = require('./models/User');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.post('/todos', (req, res)=>{
+   var todo = new Todo({'text': req.body.text})
+
+   todo.save().then((doc)=>{
+      res.send(doc)
+   }, (error)=>{
+      console.log(`some error ${error}`);
+      res.status(400).send(error);
+   })
+});
+
+var port = 3001
+app.listen(port, ()=>{
+   console.log(`started on port ${port}`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let newTodo = new Todo({
+//    text: 'Cook Dinner'
+// });
+
+// newTodo.save().then((doc) => {
+//    console.log(`doc is saved ${doc}`);
+// }, (e) => {
+//    console.log(`unable to save`);
+// });
+
+// let newUser = new User({
+//    email: 'mahaveer@email.com  '
+// });
+
+// newUser.save().then((doc) => {
+//    console.log(`user saved ${doc}`);
+// }, (e) => {
+//    console.log(`unable to save`);
+// })
+
+// mongoose.disconnect()
